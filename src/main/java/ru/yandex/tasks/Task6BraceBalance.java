fd9ccc3e804440b99d22de80af871ef5176d860c
undina <foxundina@gmail.com>
2023-08-14 20:21:58 +0300

    BraceBalance

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -1,5 +1,7 @@
 package ru.yandex.tasks;
 
+import java.util.Stack;
+
 public class Task6BraceBalance {
     public static boolean checkBalance(String sequence) {
         /*
@@ -7,6 +9,25 @@ public class Task6BraceBalance {
          * Выход: true/false, является ли строка ПСП
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+        Stack<String>stack = new Stack<>();
+        for (int i = 0; i < sequence.length(); i++) {
+            String s = sequence.substring(i, i + 1);
+            if(s.equals("(")||s.equals("[")||s.equals("{")){
+                stack.push(s);
+            }
+            else {
+                if(stack.empty()){
+                    return false;
+                }
+                if(s.equals(")") &&!stack.pop().equals("(") ||s.equals("]") &&!stack.pop().equals("[")||
+                        s.equals("}") &&!stack.pop().equals("{")){
+                    return false;
+                }
+            }
+        }
+        if(!stack.empty()){
+            return  false;
+        }
         return true;
     }
 
@@ -16,5 +37,10 @@ public class Task6BraceBalance {
 
         assert checkBalance(test1);
         assert !checkBalance(test2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkBalance("[({})]{[]}"));
+        System.out.println(checkBalance("{({})}{"));
     }
 }
